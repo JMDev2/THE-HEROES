48babce98732b17f3b6d09f94073e80dbfa7ea39
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-04-29 18:30:45 +0300

    This will add the the squad and the hero classes

---
+++ src/main/java/Squad.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Squad {
+    private int squadSize;
+    private  String squadName;
+    private String squadCause;
+    private int squadId;
+    private static List<Squad> instances = new ArrayList<Squad>();
+
+    public Squad(int squadSize, String squadName, String squadCause, int squadId) {
+        this.squadSize = squadSize;
+        this.squadName = squadName;
+        this.squadCause = squadCause;
+        this.squadId = squadId;
+    }
+
+    public int getSquadSize() {
+        return squadSize;
+    }
+
+    public void setSquadSize(int squadSize) {
+        this.squadSize = squadSize;
+    }
+
+    public String getSquadName() {
+        return squadName;
+    }
+
+    public void setSquadName(String squadName) {
+        this.squadName = squadName;
+    }
+
+    public String getSquadCause() {
+        return squadCause;
+    }
+
+    public void setSquadCause(String squadCause) {
+        this.squadCause = squadCause;
+    }
+
+    public int getSquadId() {
+        return squadId;
+    }
+
+    public void setSquadId(int squadId) {
+        this.squadId = squadId;
+    }
+
+    public static List<Squad> getInstances() {
+        return instances;
+    }
+
+    public static void setInstances(List<Squad> instances) {
+        Squad.instances = instances;
+    }
+}
