fbb8cfbbf4dc5eb92d45fd2dda1cffff1ccd9ddd
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-04-29 22:15:23 +0300

    This will add the frontend branch and the files

--- src/test/java/squadTest.java
+++ src/test/java/squadTest.java
@@ -8,4 +8,24 @@ public class squadTest {
         Squad testSquad = new Squad(3, "gitter", "water", 1);
         assertEquals(true, testSquad instanceof Squad);
     }
+    @Test
+    public void squad_instantiatesSquadsize_true() {
+        Squad testSquad = new Squad(3, "gitter", "water", 1);
+        assertEquals(3, testSquad.getSquadSize());
+    }
+    @Test
+    public void squad_instantiatesName_String() {
+        Squad testSquad = new Squad(3, "gitter", "water", 1);
+        assertEquals("gitter", testSquad.getSquadName());
+    }
+    @Test
+    public void squad_instantiatesCause_String() {
+        Squad testSquad = new Squad(3, "gitter", "water", 1);
+        assertEquals("water", testSquad.getSquadCause());
+    }
+    @Test
+    public void squad_instantiatesId_int() {
+        Squad testSquad = new Squad(3, "gitter", "water", 1);
+        assertEquals(1, testSquad.getSquadId());
+    }
 }
